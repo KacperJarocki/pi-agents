09e4f28c8db3ffd179fe71cffa45437c0591ce3d
Kacper Jarocki <dev.kacperjarocki@proton.me>
2026-01-07 20:47:46 +0100

    feat: deamonset for gateway

--- iot-ap/entrypoint.sh
+++ iot-ap/entrypoint.sh
@@ -3,9 +3,7 @@ set -eu
 
 echo "[*] iot-ap starting hostapd + dnsmasq"
 
-# dnsmasq leases w hostPath
 mkdir -p /var/lib/dnsmasq
 
-# hostapd w tle, dnsmasq na foreground (żeby pod żył)
 hostapd -dd /config/hostapd.conf &
 dnsmasq -k --conf-file=/config/dnsmasq.conf
